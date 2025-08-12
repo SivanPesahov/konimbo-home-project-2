a65f073c7ea0303b75c9e605b88e11d616a6eee5
Sivan <sivanp27540@gmail.com>
2025-08-12 11:16:07 +0300

    app.tsx changes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,14 +3,8 @@ import MainLayout from "./components/layout/MainLayout";
 import NotFoundPage from "./pages/NotFoundPage";
 import Home from "./pages/Home";
 
-// import { Provider } from "react-redux";
-// import { store } from "./store/store";
-
 function App() {
   return (
-    // -- redux
-    // <Provider store={store}>
-
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
@@ -18,8 +12,6 @@ function App() {
 
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
-
-    // </Provider>
   );
 }
 
